b8c9c4b07cb200d080e8fc2e92644a54c982490e
jessicawright <wright.983@gmail.com>
2019-03-14 11:24:04 -0400

    modal box 1 done

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,23 +1,40 @@
-var image = 1;
-showImg(image);
+// var image = 1;
+// showImg(image);
 
-// Next/previous controls
-function currentImg(n) {
-  showImg(image += n);
-}
+// // Next/previous controls
+// function currentImg(n) {
+//   showImg(image += n);
+// }
 
-function showImg(n) {
-  var i;
-  var images = document.getElementsByClassName("carousel__image");
+// function showImg(n) {
+//   var i;
+//   var images = document.getElementsByClassName("carousel__image");
  
-  if (n > images.length) {
-      image = 1
-    } 
-  if (n < 1) {
-      image = images.length
-    }
-  for (i = 0; i < images.length; i++) {
-      images[i].style.display = "none"; 
-  }
-  images[image-1].style.display = "block";   
+//   if (n > images.length) {
+//       image = 1
+//     } 
+//   if (n < 1) {
+//       image = images.length
+//     }
+//   for (i = 0; i < images.length; i++) {
+//       images[i].style.display = "none"; 
+//   }
+//   images[image-1].style.display = "block";   
+// }
+
+
+const techModal = document.querySelector('.modal__tech');
+const openModal = document.querySelector('.modal__tech--button');
+const closeModal = document.querySelector("#modal__tech--close");
+
+function showModal() {
+  // Do this CSS classes instead of changing style in JS
+  techModal.style.display = "block";
 }
+
+function hideModal() {
+  techModal.style.display = "none";
+}
+
+openModal.addEventListener('click', showModal);
+closeModal.addEventListener('click', hideModal);
