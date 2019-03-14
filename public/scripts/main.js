a5fcdc7a842704b206447b5984872444ac81695d
jessicawright <wright.983@gmail.com>
2019-03-14 15:13:06 -0400

    portfolio modals

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,40 +1,76 @@
-// var image = 1;
-// showImg(image);
+//carousel//
 
-// // Next/previous controls
-// function currentImg(n) {
-//   showImg(image += n);
-// }
+var image = 1;
+showImg(image);
 
-// function showImg(n) {
-//   var i;
-//   var images = document.getElementsByClassName("carousel__image");
+function currentImg(n) {
+  showImg(image += n);
+}
+
+function showImg(n) {
+  var i;
+  var images = document.getElementsByClassName("carousel__image");
  
-//   if (n > images.length) {
-//       image = 1
-//     } 
-//   if (n < 1) {
-//       image = images.length
-//     }
-//   for (i = 0; i < images.length; i++) {
-//       images[i].style.display = "none"; 
-//   }
-//   images[image-1].style.display = "block";   
-// }
+  if (n > images.length) {
+      image = 1
+    } 
+  if (n < 1) {
+      image = images.length
+    }
+  for (i = 0; i < images.length; i++) {
+      images[i].style.display = "none"; 
+  }
+  images[image-1].style.display = "block";   
+}
 
+//tech modal//
 
-const techModal = document.querySelector('.modal__tech');
 const openModal = document.querySelector('.modal__tech--button');
-const closeModal = document.querySelector("#modal__tech--close");
+const closeModal = document.querySelector('#modal__tech--close');
 
-function showModal() {
-  // Do this CSS classes instead of changing style in JS
-  techModal.style.display = "block";
+openModal.addEventListener('click', () => {
+  toggleShowTech(document.querySelector('.modal__tech'))
+});
+
+function toggleShowTech(element) {
+  element.classList.toggle('show__tech__modal');
 }
 
-function hideModal() {
-  techModal.style.display = "none";
+closeModal.addEventListener('click', () => {
+  toggleShowTech(document.querySelector('.modal__tech'))
+});
+
+//hospital modal//
+
+const openHospitalModal = document.querySelector('.portfolio__thumbnail');
+const closeHospitalModal = document.querySelector('#modal__hospital--close');
+
+openHospitalModal.addEventListener('click', () => {
+  toggleShowHospital(document.querySelector('.modal__hospital'))
+});
+
+function toggleShowHospital(element) {
+  element.classList.toggle('show__hospital__modal');
 }
 
-openModal.addEventListener('click', showModal);
-closeModal.addEventListener('click', hideModal);
+closeHospitalModal.addEventListener('click', () => {
+  toggleShowHospital(document.querySelector('.modal__hospital'))
+});
+
+//portfolio modal//
+
+const openHospitalModal = document.querySelector('.portfolio__thumbnail');
+const closeHospitalModal = document.querySelector('#modal__hospital--close');
+
+openHospitalModal.addEventListener('click', () => {
+  toggleShowHospital(document.querySelector('.modal__hospital'))
+});
+
+function toggleShowHospital(element) {
+  element.classList.toggle('show__hospital__modal');
+}
+
+closeHospitalModal.addEventListener('click', () => {
+  toggleShowHospital(document.querySelector('.modal__hospital'))
+});
+
