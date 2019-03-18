1fd7acaa926b4123ea62f82e5955836524e3cc94
jessicawright <wright.983@gmail.com>
2019-03-18 13:33:20 -0400

    initial

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,10 +1,16 @@
 //carousel//
 
 var image = 1;
+var text = 1;
 showImg(image);
+showContent(text);
 
 function currentImg(n) {
   showImg(image += n);
+}
+
+function currentText(n) {
+  showContent(text);
 }
 
 function showImg(n) {
@@ -14,12 +20,34 @@ function showImg(n) {
   if (n > images.length) {
     image = 1
   }
+
   if (n < 1) {
     image = images.length
   }
+
   for (i = 0; i < images.length; i++) {
     images[i].style.display = "none";
   }
+
+  images[image - 1].style.display = "block";
+}
+
+function showContent(n) {
+  var i;
+  var images = document.getElementsByClassName("carousel__image");
+
+  if (n > images.length) {
+    image = 1
+  }
+
+  if (n < 1) {
+    image = images.length
+  }
+
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+
   images[image - 1].style.display = "block";
 }
 
