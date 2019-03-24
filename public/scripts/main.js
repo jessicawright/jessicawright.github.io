8bc513b22d18a4bfa5000f84a83d4a415f32e8f8
jessicawright <wright.983@gmail.com>
2019-03-24 13:01:54 -0400

    mobile-first design restart

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,189 +1,197 @@
-//carousel//
+// //carousel//
 
-var image = 1;
-currentImg(image);
+// var image = 1;
+// currentImg(image);
 
-function currentImg(n) {
-  showImg(image += n);
-}
+// function currentImg(n) {
+//   showImg(image += n);
+// }
 
-function showImg(n) {
-  var i;
-  var images = document.getElementsByClassName("carousel__image");
-  
-  if (n > images.length) {
-    image = 1
-  }
-  
-  if (n < 1) {
-    image = images.length
-  }
-  
-  for (i = 0; i < images.length; i++) {
-    images[i].style.display = "none";
-  }
-  
-  images[image - 1].style.display = "block";
-}
+// function showImg(n) {
+//   var i;
+//   var images = document.getElementsByClassName("carousel__image");
 
-var text = 1;
-currentText(text);
+//   if (n > images.length) {
+//     image = 1
+//   }
 
-function currentText(n) {
-  showContent(text += n);
-}
+//   if (n < 1) {
+//     image = images.length
+//   }
 
-function showContent(n) {
-  var i;
-  var texts = document.getElementsByClassName("carousel__content");
-  
-  if (n > texts.length) {
-    text = 1
-  }
-  
-  if (n < 1) {
-    text = texts.length
-  }
-  
-  for (i = 0; i < text.length; i++) {
-    texts[i].style.display = "none";
-  }
+//   for (i = 0; i < images.length; i++) {
+//     images[i].style.display = "none";
+//   }
 
-  texts[text - 1].style.display = "block";
-}
+//   images[image - 1].style.display = "block";
+// }
 
-//tech modal//
+// var text = 1;
+// currentText(text);
 
-const openModal = document.querySelector('.modal__tech--button');
-const closeModal = document.querySelector('#modal__tech--close');
+// function currentText(n) {
+//   showContent(text += n);
+// }
 
-openModal.addEventListener('click', () => {
-  toggleShowTech(document.querySelector('.modal__tech'))
-});
+// function showContent(n) {
+//   var i;
+//   var texts = document.getElementsByClassName("carousel__content");
 
-function toggleShowTech(element) {
-  element.classList.toggle('show__tech__modal');
-}
+//   if (n > texts.length) {
+//     text = 1
+//   }
 
-closeModal.addEventListener('click', () => {
-  toggleShowTech(document.querySelector('.modal__tech'))
-});
+//   if (n < 1) {
+//     text = texts.length
+//   }
 
-//hospital modal//
+//   for (i = 0; i < text.length; i++) {
+//     texts[i].style.display = "none";
+//   }
 
-const openHospitalModal = document.querySelector('#portfolio__thumbnail__hospital');
-const closeHospitalModal = document.querySelector('#modal__hospital--close');
+//   texts[text - 1].style.display = "block";
+// }
 
-openHospitalModal.addEventListener('click', () => {
-  toggleShowModal(document.querySelector('.modal__hospital'))
-});
+// //tech modal//
 
-closeHospitalModal.addEventListener('click', () => {
-  toggleShowModal(document.querySelector('.modal__hospital'))
-});
+// const openModal = document.querySelector('.modal__tech--button');
+// const closeModal = document.querySelector('#modal__tech--close');
 
-//portfolio modal//
-
-const openPortfolioModal = document.querySelector('#portfolio__thumbnail__portfolio');
-const closePortfolioModal = document.querySelector('#modal__portfolio--close');
-
-openPortfolioModal.addEventListener('click', () => {
-  toggleShowModal(document.querySelector('.modal__portfolio'))
-});
-
-closePortfolioModal.addEventListener('click', () => {
-  toggleShowModal(document.querySelector('.modal__portfolio'))
-});
-
-//pet modal//
-
-const openPetModal = document.querySelector('#portfolio__thumbnail__pet');
-const closePetModal = document.querySelector('#modal__pet--close');
-
-openPetModal.addEventListener('click', () => {
-  toggleShowModal(document.querySelector('.modal__pet'))
-});
-
-closePetModal.addEventListener('click', () => {
-  toggleShowModal(document.querySelector('.modal__pet'))
-});
-
-//toggle function//
-function toggleShowModal(element) {
-  element.classList.toggle('show__modal');
-};
-
-//accordion action//
-const hospitalSkillsBar = document.querySelector('#hospital-skills');
-const hospitalChallengesBar = document.querySelector('#hospital-challenges');
-const hospitalNextBar = document.querySelector('#hospital-next');
-const portfolioSkillsBar = document.querySelector('#portfolio-skills');
-const portfolioChallengesBar = document.querySelector('#portfolio-challenges');
-const portfolioNextBar = document.querySelector('#portfolio-next');
-const petSkillsBar = document.querySelector('#pet-skills');
-const petChallengesBar = document.querySelector('#pet-challenges');
-const petNextBar = document.querySelector('#pet-next');
-
-hospitalSkillsBar.addEventListener('click', () => {
-  toggleAccordion(document.querySelector('#hospital-skills-panel'))
-});
-
-hospitalChallengesBar.addEventListener('click', () => {
-  toggleAccordion(document.querySelector('#hospital-challenge-panel'))
-});
-
-hospitalNextBar.addEventListener('click', () => {
-  toggleAccordion(document.querySelector('#hospital-next-panel'))
-});
-
-portfolioSkillsBar.addEventListener('click', () => {
-  toggleAccordion(document.querySelector('#portfolio-skills-panel'))
-});
-
-portfolioChallengesBar.addEventListener('click', () => {
-  toggleAccordion(document.querySelector('#portfolio-challenge-panel'))
-});
-
-portfolioNextBar.addEventListener('click', () => {
-  toggleAccordion(document.querySelector('#portfolio-next-panel'))
-});
-
-petSkillsBar.addEventListener('click', () => {
-  toggleAccordion(document.querySelector('#pet-skills-panel'))
-});
-
-petChallengesBar.addEventListener('click', () => {
-  toggleAccordion(document.querySelector('#pet-challenge-panel'))
-});
-
-// petNextBar.addEventListener('click', () => {
-//   toggleAccordion(document.querySelector('#pet-next-panel'))
+// openModal.addEventListener('click', () => {
+//   toggleShowTech(document.querySelector('.modal__tech'))
 // });
 
-//toggle function//
-function toggleAccordion(element) {
-  element.classList.toggle('accordion__panel--show');
-};
+// function toggleShowTech(element) {
+//   element.classList.toggle('show__tech__modal');
+// }
 
-//drop-down nav//
+// closeModal.addEventListener('click', () => {
+//   toggleShowTech(document.querySelector('.modal__tech'))
+// });
+
+// //hospital modal//
+
+// const openHospitalModal = document.querySelector('#portfolio__thumbnail__hospital');
+// const closeHospitalModal = document.querySelector('#modal__hospital--close');
+
+// openHospitalModal.addEventListener('click', () => {
+//   toggleShowModal(document.querySelector('.modal__hospital'))
+// });
+
+// closeHospitalModal.addEventListener('click', () => {
+//   toggleShowModal(document.querySelector('.modal__hospital'))
+// });
+
+// //portfolio modal//
+
+// const openPortfolioModal = document.querySelector('#portfolio__thumbnail__portfolio');
+// const closePortfolioModal = document.querySelector('#modal__portfolio--close');
+
+// openPortfolioModal.addEventListener('click', () => {
+//   toggleShowModal(document.querySelector('.modal__portfolio'))
+// });
+
+// closePortfolioModal.addEventListener('click', () => {
+//   toggleShowModal(document.querySelector('.modal__portfolio'))
+// });
+
+// //pet modal//
+
+// const openPetModal = document.querySelector('#portfolio__thumbnail__pet');
+// const closePetModal = document.querySelector('#modal__pet--close');
+
+// openPetModal.addEventListener('click', () => {
+//   toggleShowModal(document.querySelector('.modal__pet'))
+// });
+
+// closePetModal.addEventListener('click', () => {
+//   toggleShowModal(document.querySelector('.modal__pet'))
+// });
+
+// //toggle function//
+// function toggleShowModal(element) {
+//   element.classList.toggle('show__modal');
+// };
+
+// //accordion action//
+// const hospitalSkillsBar = document.querySelector('#hospital-skills');
+// const hospitalChallengesBar = document.querySelector('#hospital-challenges');
+// const hospitalNextBar = document.querySelector('#hospital-next');
+// const portfolioSkillsBar = document.querySelector('#portfolio-skills');
+// const portfolioChallengesBar = document.querySelector('#portfolio-challenges');
+// const portfolioNextBar = document.querySelector('#portfolio-next');
+// const petSkillsBar = document.querySelector('#pet-skills');
+// const petChallengesBar = document.querySelector('#pet-challenges');
+// const petNextBar = document.querySelector('#pet-next');
+
+// hospitalSkillsBar.addEventListener('click', () => {
+//   toggleAccordion(document.querySelector('#hospital-skills-panel'))
+// });
+
+// hospitalChallengesBar.addEventListener('click', () => {
+//   toggleAccordion(document.querySelector('#hospital-challenge-panel'))
+// });
+
+// hospitalNextBar.addEventListener('click', () => {
+//   toggleAccordion(document.querySelector('#hospital-next-panel'))
+// });
+
+// portfolioSkillsBar.addEventListener('click', () => {
+//   toggleAccordion(document.querySelector('#portfolio-skills-panel'))
+// });
+
+// portfolioChallengesBar.addEventListener('click', () => {
+//   toggleAccordion(document.querySelector('#portfolio-challenge-panel'))
+// });
+
+// portfolioNextBar.addEventListener('click', () => {
+//   toggleAccordion(document.querySelector('#portfolio-next-panel'))
+// });
+
+// petSkillsBar.addEventListener('click', () => {
+//   toggleAccordion(document.querySelector('#pet-skills-panel'))
+// });
+
+// petChallengesBar.addEventListener('click', () => {
+//   toggleAccordion(document.querySelector('#pet-challenge-panel'))
+// });
+
+// // petNextBar.addEventListener('click', () => {
+// //   toggleAccordion(document.querySelector('#pet-next-panel'))
+// // });
+
+// //toggle function//
+// function toggleAccordion(element) {
+//   element.classList.toggle('accordion__panel--show');
+// };
+
+// //drop-down nav//
 
 run();
 
 function run() {
-  bindNavButtonToNavList();
+    bindNavButtonToNavList();
 }
 
-function toggleHide(element) {
-  element.classList.toggle('hide');
+function toggleShow(element) {
+    element.classList.toggle('show');
 }
 
 function bindNavButtonToNavList() {
 
-  const navButton = document.querySelector('#nav__plus');
+    const navButton = document.querySelector('#nav__plus');
 
-  navButton.addEventListener('click', function () {
-  const navList = document.querySelector('.nav__small__dropdown');
-  toggleHide(navList);
-});
+    navButton.addEventListener('click', function () {
+        const navList = document.querySelector('.nav__hidden');
+        toggleShow(navList);
 
+    });
+
+    window.addEventListener('mouseup', function(event) {
+        const navList = document.querySelector('.nav__hidden');
+        if(event.target != navList && event.target.parentNode != navList) {
+            navList.classList.remove('show');
+        }
+    
+    });
 }
